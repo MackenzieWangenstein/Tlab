f8f34f7b7d8d12f7e33e77dce0c8bb2d58859dd1
MackenzieWangenstein <mklitzke82@gmail.com>
2018-03-21 16:07:11 -0700

    cleaned up package

--- main.py
+++ main.py
@@ -6,20 +6,15 @@ import sys
 
 
 def main():
-	# create random string
-	# make training data from string
 	if sys.argv.__contains__("-bc"):
 		random_decimal = random.randrange(256, 516)
 		binarycreator.create_and_classify_binary_set("rgflclassifier/random-binary.txt", random_decimal)
 
+	if sys.argv.__contains__("-hdgen"):
+		hdg.create_data(10, 1000, "hammingdistanceclassifier/hammingdata.csv")
 
-	#TODO: add sys arg
-	# train data model.
-	# if sys.argv.__contains__("-hdg"):
-	# hdg.create_data(10, 1000, "hammingdistanceclassifier/hammingdata.csv")
-
-	hdcrunner.run()
-
+	if sys.argv.__contains__("-hdc"):
+		hdcrunner.run()
 
 
 if __name__ == '__main__':
